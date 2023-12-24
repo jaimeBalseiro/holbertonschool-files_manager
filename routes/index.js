e41610aa1a770032024b99a4213d105e95e7bd6b
Jaime Diaz Balseiro <105442363+jaimeBalseiro@users.noreply.github.com>
2023-12-24 07:49:47 +0000

    eslint fix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
-const AuthController = require('../controllers/AuthController')
+const AuthController = require('../controllers/AuthController');
 
 const router = express.Router();
 
