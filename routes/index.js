71e64cd7f1cf94f90791e357fbac9ca94d7fb6bc
Jaime Diaz Balseiro <105442363+jaimeBalseiro@users.noreply.github.com>
2023-12-24 08:35:48 +0000

    test

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,9 @@ router.get('/stats', AppController.getStats);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.get('/files/:id/data', FilesController.getFile);
 // POST's
 router.post('/users', UsersController.postNew);
 router.post('/files', FilesController.postUpload);
